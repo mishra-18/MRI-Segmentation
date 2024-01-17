7be6563a698e7d6fb318e7fda4cf58be1c146be7
mishra-18 <mishra4475@gmail.com>
2024-01-17 14:53:58 +0530

    updated files

--- config/configure.py
+++ config/configure.py
@@ -1,4 +1,14 @@
 import os
+"""
+If you are planing to train the model. The data_ingestion stage requires
+a kaggle API key and your user name. That is why you need to have 
+.kaggle/ folder under config.
+config-|
+       |-.kaggle-|         
+                 |-kaggle.json
+       |-configure.py      
+Make sure you replace your username under kaggle.json. 
+"""
 mask_images_path = os.getcwd() + "/data/lgg-mri-segmentation/kaggle_3m/*/*_mask.tif"
 SAVE_MODEL_PATH = os.getcwd() + "/src/model/best_model.pth" 
 SAVE_DATA_PATH = os.getcwd() + "/data"
